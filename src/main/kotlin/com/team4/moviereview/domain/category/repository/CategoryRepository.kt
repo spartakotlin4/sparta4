2cba9b6f3215e90814867015de4d5577bf8b0b3f
Alice <hifumialice@gmail.com>
2024-07-03 00:40:48 +0900

    ✨ feat: create category repository

---
+++ src/main/kotlin/com/team4/moviereview/domain/category/repository/CategoryRepository.kt
@@ -0,0 +1,8 @@
+package com.team4.moviereview.domain.category.repository
+
+import com.team4.moviereview.domain.category.model.Category
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CategoryRepository : JpaRepository<Category, Long> {
+
+}
