9bbc913252d8677fa99379ec7d44c60bba467854
Alice <hifumialice@gmail.com>
2024-07-03 00:42:58 +0900

    ✨ feat: request and response dto create

---
+++ src/main/kotlin/com/team4/moviereview/domain/category/dto/request/CategoryCreateRequest.kt
@@ -0,0 +1,5 @@
+package com.team4.moviereview.domain.category.dto.request
+
+data class CategoryCreateRequest(
+    val name: String
+)
---
+++ src/main/kotlin/com/team4/moviereview/domain/category/dto/request/CategoryUpdateRequest.kt
@@ -0,0 +1,5 @@
+package com.team4.moviereview.domain.category.dto.request
+
+data class CategoryUpdateRequest(
+    val name: String
+)
