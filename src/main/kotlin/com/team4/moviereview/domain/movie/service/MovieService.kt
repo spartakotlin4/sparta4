d2bbb33f5808c5e74b4405ab09947df10e0301eb
pcouqc <nagapen@nate.com>
2024-07-03 17:23:56 +0900

    create : create interface service

--- src/main/kotlin/com/team4/moviereview/domain/movie/service/MovieService.kt
+++ src/main/kotlin/com/team4/moviereview/domain/movie/service/MovieService.kt
@@ -1,4 +1,18 @@
 package com.team4.moviereview.domain.movie.service
 
+import com.team4.moviereview.domain.movie.dto.FilterRequest
+import com.team4.moviereview.domain.movie.dto.MovieDetailResponse
+import com.team4.moviereview.domain.movie.dto.MovieResponse
+import com.team4.moviereview.domain.movie.dto.SearchRequest
+import org.springframework.stereotype.Service
+
+@Service
 interface MovieService {
+    fun getMovieList(): List<MovieResponse>
+
+    fun getMovieDetails(movieId: Long): MovieDetailResponse
+
+    fun searchMovies(request: SearchRequest): MovieResponse
+
+    fun filterMovies(request: FilterRequest): MovieResponse
 }
