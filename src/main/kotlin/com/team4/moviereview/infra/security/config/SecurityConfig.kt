7a9592e27453b60e63f9c70b143366f3ca617814
Alice <hifumialice@gmail.com>
2024-07-04 16:22:26 +0900

    :recycle: refactor: 이메일 null뜨는 부분 수정

--- src/main/kotlin/com/team4/moviereview/infra/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/team4/moviereview/infra/security/config/SecurityConfig.kt
@@ -3,16 +3,20 @@ package com.team4.moviereview.infra.security.config
 import com.team4.moviereview.infra.security.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 class SecurityConfig(
     private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+    private val authenticationEntrypoint: AuthenticationEntryPoint
 ) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -31,7 +35,9 @@ class SecurityConfig(
                     .anyRequest().authenticated()
             }
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
-
+            .exceptionHandling{
+                it.authenticationEntryPoint(authenticationEntrypoint)
+            }
             .build()
 
 
