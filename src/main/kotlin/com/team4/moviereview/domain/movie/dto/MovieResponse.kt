d1684d550f32a805537903e9fdcd9d0e3b4ac29a
taehuiii <swanna18@naver.com>
2024-07-05 19:11:41 +0900

    :bug: fix:  update search api ( response dto 주석 제거)

--- src/main/kotlin/com/team4/moviereview/domain/movie/dto/MovieResponse.kt
+++ src/main/kotlin/com/team4/moviereview/domain/movie/dto/MovieResponse.kt
@@ -10,21 +10,6 @@ data class MovieResponse(
     val category: List<String>,
     val releaseDate: LocalDate,
     val rating: Double,
-) {
-
-    companion object {
-//        fun from(movie: MovieData, categories: List<Category>): MovieResponse {
-//            return MovieResponse(
-//                id = movie.id,
-//                title = movie.title,
-//                director = movie.director,
-//                actors = movie.actors,
-//                category = categories,
-//                releaseDate = movie.releaseDate,
-//                rating = movie.rating,
-//            )
-//        }
-    }
-}
+)
 
 
