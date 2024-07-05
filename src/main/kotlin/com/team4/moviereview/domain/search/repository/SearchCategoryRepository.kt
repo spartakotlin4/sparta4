860f8d014283e2daf2a4332b13cf345e143f90c8
Alice <hifumialice@gmail.com>
2024-07-05 19:48:15 +0900

    :sparkles: feat: 인기 검색어, 카테고리 구현

--- src/main/kotlin/com/team4/moviereview/domain/search/repository/SearchCategoryRepository.kt
+++ src/main/kotlin/com/team4/moviereview/domain/search/repository/SearchCategoryRepository.kt
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository
 @Repository
 interface SearchCategoryRepository: JpaRepository<SearchCategory, Long> {
 
-    @Query("select c.category.name, count(c) as cnt from SearchCategory c group by c.category order by cnt desc, c.category.name desc limit :rankLimit")
+    @Query("select new com.team4.moviereview.domain.search.dto.SearchCategoryResponse (c.category.name, count(c)) from SearchCategory c group by c.category order by count(c) desc, c.category.name desc limit :rankLimit")
     fun findAllByLimit(rankLimit: Int): List<SearchCategoryResponse>
 }
--- src/main/kotlin/com/team4/moviereview/domain/search/repository/SearchWordRepository.kt
+++ src/main/kotlin/com/team4/moviereview/domain/search/repository/SearchWordRepository.kt
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository
 @Repository
 interface SearchWordRepository: JpaRepository<SearchWord, Int> {
 
-    @Query("select s.keyword, count(s) as cnt from SearchWord s group by s.keyword order by cnt desc, s.keyword desc limit :limitRank")
+    @Query("select new com.team4.moviereview.domain.search.dto.SearchWordResponse( s.keyword, count(s)) from SearchWord s group by s.keyword order by count(s) desc, s.keyword desc limit :limitRank")
     fun findAllByLimit(limitRank: Int): List<SearchWordResponse>
 }
