86bcf92a3aeb4f30dbf248118db1cf6476607d51
pcouqc <nagapen@nate.com>
2024-07-05 10:46:22 +0900

    :sparkles: feat : add methods for popular searching keyword save

--- src/main/kotlin/com/team4/moviereview/domain/search/service/SearchService.kt
+++ src/main/kotlin/com/team4/moviereview/domain/search/service/SearchService.kt
@@ -1,10 +1,14 @@
 package com.team4.moviereview.domain.search.service
 
+import com.team4.moviereview.domain.category.model.Category
 import com.team4.moviereview.domain.search.dto.SearchCategoryResponse
 import com.team4.moviereview.domain.search.dto.SearchWordResponse
+import com.team4.moviereview.domain.search.model.SearchCategory
+import com.team4.moviereview.domain.search.model.SearchWord
 import com.team4.moviereview.domain.search.repository.SearchCategoryRepository
 import com.team4.moviereview.domain.search.repository.SearchWordRepository
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 
 @Service
 class SearchService(
@@ -19,5 +23,13 @@ class SearchService(
 
     fun getPopularCategory(): List<SearchCategoryResponse> {
         return searchCategoryRepository.findAllByLimit(rankLimit)
+    }
+
+    fun saveSearchedKeyword(keyword: String) {
+        searchWordRepository.save(SearchWord(keyword, LocalDate.now()))
+    }
+
+    fun saveSearchedCategory(category: Category) {
+        searchCategoryRepository.save(SearchCategory(category, LocalDate.now()))
     }
 }
