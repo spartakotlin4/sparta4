b00e10170f1361d82ee4de5ad635b9d799b9ccd1
Alice <hifumialice@gmail.com>
2024-07-08 16:56:00 +0900

    :sparkles: feat: enable caching

--- src/main/kotlin/com/team4/moviereview/MovieReviewApplication.kt
+++ src/main/kotlin/com/team4/moviereview/MovieReviewApplication.kt
@@ -2,10 +2,12 @@ package com.team4.moviereview
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableCaching
 class MovieReviewApplication
 
 fun main(args: Array<String>) {
